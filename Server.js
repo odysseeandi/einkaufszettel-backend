e0cd29b0084414f34bef74c08db88999b3326f5b
odysseeandi <andreaskrausser@googlemail.com>
2024-05-26 17:28:16 +0200

    4

--- Server.js
+++ Server.js
@@ -10,7 +10,11 @@ const app = express()
 const PORT = process.env.port || 5000
 
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+    origin: ["https://einkaufszettel-client.vercel.app/"],
+    methods: ["GET", "POST"],
+    credentials: true
+}))
 
 mongoose
 .connect(process.env.MONGODB_URL)
